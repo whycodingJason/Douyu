f0c060f6bb90722391513c638df4dd1b2ebe33e5
王永生 <1948396116@qq.com>
2016-09-23 09:40:56 +0800

    用SB搭好了框架

---
+++ DouYuZhiBo/DouYuZhiBo/Class/Follow/FollowViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FollowViewController.swift
+//  DouYuZhiBo
+//
+//  Created by 王永生 on 16/9/23.
+//  Copyright © 2016年 王永生. All rights reserved.
+//
+
+import UIKit
+
+class FollowViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
